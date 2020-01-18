bab6b2689ff4c90fc782136112477e2e8c75c397
kaigikinyua <kaigikinyua@gmail.com>
2020-01-18 12:28:47 +0300

    blog not found message

--- static/js/bloglist.js
+++ static/js/bloglist.js
@@ -3,14 +3,23 @@ var blogs=document.querySelectorAll('.blogTile');
 var search=document.getElementById('search');
 search.addEventListener('keyup',function(){
     var text=document.getElementById('search').value;
-   blogs.forEach(tile=>{
-       var title_cover=tile.querySelector('h3.title');
-       var title=title_cover.innerHTML;
-       var author=tile.querySelector('i.auhor');
-        if(title.toLowerCase().indexOf(text.toLowerCase())!=-1){
-            tile.style.display="block";
-        }else{
-            tile.style.display="none";
-        }
-   }) 
+    var found=0
+    blogs.forEach(tile=>{
+        var title_cover=tile.querySelector('h3.title');
+        var title=title_cover.innerHTML;
+        var author=tile.querySelector('i.auhor');
+         if(title.toLowerCase().indexOf(text.toLowerCase())!=-1){
+             tile.style.display="block";
+             found=1
+         }else{
+             tile.style.display="none";
+         }
+    });
+    if(found==0){
+        var notfound=document.querySelector('.notfound');
+        notfound.style.display="block"
+    }else{
+        var notfound=document.querySelector('.notfound');
+        notfound.style.display="none"
+    }
 });
